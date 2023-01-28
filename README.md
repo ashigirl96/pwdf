8ddd76e5bc40514a219384006c9e4e9e062508f9
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2023-01-28 17:46:58 +0900

    fix tiny

---
+++ README.md
@@ -0,0 +1,8 @@
+# pwdf
+
+join current path with given path
+
+```bash
+> pwdf ../pwdf/Cargo.lock
+/Users/ashigirl96/.ghq/src/github.com/ashigirl96/pwdf/Cargo.lock
+```
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use anyhow::ensure;
 use std::env;
+use std::path::{Path, PathBuf};
 
 fn validate(s: &Vec<String>) -> anyhow::Result<()> {
-    ensure!(s.len() >= 2, "Give the argument file name");
+    ensure!(
+        s.len() == 2,
+        "pwdf takes 1 positional argument but 2 were given"
+    );
     Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    validate(&args)?;
     let current_dir = env::current_dir()?;
-    let file_name = current_dir.join(&args[1]);
-    let file_name_str = file_name.into_os_string().into_string().unwrap();
-    println!("{file_name_str}");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("{}", current_dir.to_string_lossy());
+        return Ok(());
+    }
+    validate(&args)?;
+    let target_relative_path = Path::new(&args[1]);
+    let file_name = current_dir.join(target_relative_path);
+    let canonicalize = file_name.canonicalize()?;
+    let file_path = canonicalize.to_string_lossy();
+    println!("{file_path}");
     Ok(())
 }
